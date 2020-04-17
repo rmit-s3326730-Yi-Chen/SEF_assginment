274646b83d13dd11339fd64fb13d97a786703ff9
rmit-s3805274-rupadivya-kethineni <62740155+rmit-s3805274-rupadivya-kethineni@users.noreply.github.com>
2020-04-17 18:09:50 +1000

    Update JobOffer.java

--- SEF assign/src/assign/JobOffer.java
+++ SEF assign/src/assign/JobOffer.java
@@ -1,27 +1,52 @@
 package assign;
 
 public class JobOffer {
-  private Type type;
+  private Type OfferType;
   
-  public boolean setType(int typeChecker) {
-		if(typeChecker ==0) {
-			this.type=Type.Local;
-			System.out.println("Offer for local student");
-			return true;
-		}
-		else if(typeChecker ==1) {
-			this.type =Type.International;
-			System.out.println("Offer for international student");
-			return true;
-		}
-		else {
-			System.out.println("The input need to be 0 for Local, 1 for international");
-			return false;
-		}
+	public Type getOfferType() {
+		return this.OfferType;
 	}
-  
-	public Type getOfferTypes() {
-		return this.type;
+	
+	private String Title;
+	private String Description;
+	
+	public JobOffer(String Title, String Description, Double Wage, Type OfferType) {
+		this.Title = Title;
+		this.Description  = Description;
+		this.Wage = Wage;
+		this.OfferType = OfferType;
+	}
+
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String Title) {
+		this.Title = Title;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String Description) {
+		this.Description = Description;
+	}
+	
+	public double setWage(Double Wage) {
+		this.Wage = Wage;
+	}
+	
+	public void getWage() {
+		return Wage;
+	}
+	
+	public void setOfferType(Type OfferType) {
+		this.OfferType = OfferType;
+	}
+	
+	public Type getOfferType() {
+		return OfferType;
 	}
 
 }
