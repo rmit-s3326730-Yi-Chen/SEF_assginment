92059fc8623ff330a3aef2f7cb35d259b19a1135
rmit-s3805274-rupadivya-kethineni <62740155+rmit-s3805274-rupadivya-kethineni@users.noreply.github.com>
2020-04-17 18:32:50 +1000

    Update JobOffer.java

--- SEF assign/src/assign/JobOffer.java
+++ SEF assign/src/assign/JobOffer.java
@@ -37,9 +37,21 @@ public class JobOffer {
 		return Wage;
 	}
 	
-	public void setOfferType(Type OfferType) {
-		this.OfferType = OfferType;
-	}
+	public boolean setOfferType(int typeChecker) {
+		if(typeChecker ==0) {
+			this.OfferType=Type.Local;
+			System.out.println("Offer for local student");
+			return true;
+		}
+		else if(typeChecker ==1) {
+			this.OfferType =Type.International;
+			System.out.println("Offer for international student");
+			return true;
+		}
+		else {
+			System.out.println("The input need to be 0 for Local, 1 for international");
+			return false;
+		}
 	
 	public Type getOfferType() {
 		return OfferType;
