116fd81b07a5b42ad125b3fb626772de78923eb3
rmit-s3805274-rupadivya-kethineni <62740155+rmit-s3805274-rupadivya-kethineni@users.noreply.github.com>
2020-05-06 00:32:25 +1000

    Update JobOffer.java

--- SEF assign/src/assign/JobOffer.java
+++ SEF assign/src/assign/JobOffer.java
@@ -3,19 +3,21 @@ package assign;
 import java.util.ArrayList;
 
 public class JobOffer {
-	private Type OfferType;
-	private String Title;
-	private String Description;
-	private double Wage;
-	private Status Status;
+	private static Type OfferType;
+	private static String Title;
+	private static String Username;
+	private static String Description;
+	private static double Wage;
+	private static Status Status;
 	private ArrayList<JobOffer> offers = new ArrayList<>();
-	
-	public JobOffer(String Title, String Description, Double Wage, Type OfferType) {
+
+	public JobOffer(String Title, String Description, String Username, Double Wage, Type OfferType) {
 		this.Title = Title;
 		this.Description = Description;
+		this.Username = Username;
 		this.Wage = Wage;
 		this.OfferType = OfferType;
-		this.Status= Status.Available;
+		this.Status = Status.Available;
 	}
 
 	public String getTitle() {
@@ -32,6 +34,14 @@ public class JobOffer {
 
 	public void setDescription(String Description) {
 		this.Description = Description;
+	}
+	
+	public String getUsername() {
+		return Username;
+	}
+
+	public void setUsername(String Username) {
+		this.Username = Username;
 	}
 
 	public double setWage(Double Wage) {
@@ -50,13 +60,23 @@ public class JobOffer {
 	public Type getOfferType() {
 		return OfferType;
 	}
-	
+
 	public Status getStatus() {
 		return Status;
 	}
-	
+
 	public void setStatus(Status Status) {
 		this.Status = Status;
 	}
 
+	public static String getJobOffer() {
+		String print = "\n";
+		print = print + "Title: " + Title + "\n";
+		print = print + "Description: " + Description + "\n";
+		print = print + "Creator: " + Username + "\n";
+		print = print + "Wage per hour: " + Wage + "\n";
+		print = print + "Type of Applicant: " + OfferType + "\n";
+		print = print + "Status: " + Status + "\n";
+		return print;
+	}
 }
