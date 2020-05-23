4d95cc1e006053c1f601497b60f6389fd5443a24
rmit-s3805274-rupadivya-kethineni <62740155+rmit-s3805274-rupadivya-kethineni@users.noreply.github.com>
2020-05-24 05:27:02 +1000

    Update Interview.java

--- SEF assign/src/assign/Interview.java
+++ SEF assign/src/assign/Interview.java
@@ -3,5 +3,79 @@ package assign;
 import java.util.ArrayList;
 
 public class Interview {
+	private String Title;
+	private String User;
+	private String Description;
+	private String Venue;
+	private String Time;
+	private String Result;
 	private ArrayList<Interview> interviews = new ArrayList<Interview>();
+
+	public Interview(String User, String Title, String Description, String Venue, String Time) {
+		this.User = User;
+		this.Title = Title;
+		this.Description = Description;
+		this.Venue = Venue;
+		this.Time = Time;
+	}
+
+	public String getUser() {
+		return User;
+	}
+
+	public void setUser(String User) {
+		this.User = User;
+	}
+
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String Title) {
+		this.Title = Title;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String Description) {
+		this.Description = Description;
+	}
+
+	public void setVenue(String Venue) {
+		this.Venue = Venue;
+	}
+
+	public String getVenue() {
+		return Venue;
+	}
+
+	public void setTime(String Time) {
+		this.Time = Time;
+	}
+
+	public String getTime() {
+		return Time;
+	}
+
+	public void setResult(String Result) {
+		this.Result = Result;
+	}
+
+	public String getResult() {
+		return Result;
+	}
+
+	public String getInterview() {
+		String print = "\n";
+		print = "The Interview Details are: ";
+		print = print + "Applicant Username: " + User + "\n";
+		print = print + "Title: " + Title + "\n";
+		print = print + "Description: " + Description + "\n";
+		print = print + "Venue: " + Venue + "\n";
+		print = print + "Time: " + Time + "\n";
+		return print;
+	}
+
 }
