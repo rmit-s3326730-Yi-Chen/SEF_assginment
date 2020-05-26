94673c1ac2b7b8f73dca0741f55b46da2b4407ab
rmit-s3805274-rupadivya-kethineni <62740155+rmit-s3805274-rupadivya-kethineni@users.noreply.github.com>
2020-05-27 01:38:29 +1000

    Update Interview.java

--- SEF assign/src/assign/Interview.java
+++ SEF assign/src/assign/Interview.java
@@ -1,22 +1,35 @@
 package assign;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Interview implements Serializable{
-	private String Title;
+	public enum Outcome {
+		Called,
+		Noappearance,
+		Success,
+		Fail;
+	}
+	
+	private static String Title;
 	private String User;
-	private String Description;
+	private static String Description;
 	private String Venue;
-	private String Time;
-	private String Result;
+	private LocalTime Time;
+	private String Username;
+	private Outcome Result;
+	private LocalDate Date;
 
-
-	public Interview(String User, String Title, String Description, String Venue, String Time) {
+	public Interview(String User, String Title, String Description, String Venue, LocalDate Date, LocalTime Time, String Username, Outcome Result) {
 		this.User = User;
 		this.Title = Title;
 		this.Description = Description;
 		this.Venue = Venue;
+		this.Date = Date;
 		this.Time = Time;
+		this.Result = Outcome.Called;
+		this.Username = Username;
 	}
 
 	public String getUser() {
@@ -51,32 +64,48 @@ public class Interview implements Serializable{
 		return Venue;
 	}
 
-	public void setTime(String Time) {
+	public void setDate(LocalDate Date) {
+		this.Date = Date;
+	}
+
+	public LocalDate getDate() {
+		return Date;
+	}
+
+	public void setTime(LocalTime Time) {
 		this.Time = Time;
 	}
 
-	public String getTime() {
+	public LocalTime getTime() {
 		return Time;
 	}
-	
 
-	public void setResult(String Result) {
+	public String getUsername() {
+		return Username;
+	}
+
+	public void setUsername(String Username) {
+		this.Username = Username;
+	}
+	
+	public void setResult(Outcome Result) {
 		this.Result = Result;
 	}
 
-	public String getResult() {
+	public Outcome getResult() {
 		return Result;
 	}
 
 	public String getInterview() {
 		String print = "\n";
-		print = "The Interview Details are: \n";
+		print = "The Interview Details are \n";
 		print = print + "Applicant Username: " + User + "\n";
 		print = print + "Title: " + Title + "\n";
 		print = print + "Description: " + Description + "\n";
 		print = print + "Venue: " + Venue + "\n";
-		print = print + "Time: " + Time ;
+		print = print + "Date: " + Date + "\n";
+		print = print + "Time: " + Time + "\n";
+		print = print + "Creator: " + Username + "\n";
 		return print;
 	}
-
 }
